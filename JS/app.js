4104590ffc0a3177db86ed339b282fab9abe7b60
Heba1998 <hebaalmomani1998@gmail.com>
2021-04-26 23:20:18 +0300

    finished my lab

---
+++ JS/app.js
@@ -0,0 +1,91 @@
+ 'use strict';
+
+ let username =prompt('what is your name ?');
+ console.log(username);
+ alert(" Hello " + username + " ! My name is Heba, Let's start with a short quiz not to determine your level but only to increase your knowledge ^.^");
+
+ let Q1 = prompt('Does the computer understand human language?');
+
+ switch (Q1.toUpperCase()) {
+     case 'NO','N':
+         console.log('Correct answer ');
+         alert("correct answer");
+         break;
+    
+     case 'YES','Y':
+         console.log('Wrong answer');
+         alert("Wrong answer");
+         break;
+    
+ }
+
+
+ let Q2 = prompt('Is 1 yes or no in binary?');
+
+ switch (Q2.toUpperCase()) {
+     case 'YES','Y':
+         console.log('Correct answer ');
+         alert("correct answer");
+         break;
+    
+     case 'NO','N':
+         console.log('Wrong answer');
+         alert("Wrong answer");
+         break;
+    
+ }
+
+
+ let Q3 = prompt('2 is 10 in binary?');
+
+ switch (Q3.toUpperCase()) {
+     case 'YES','Y':
+         console.log('Correct answer ');
+         alert("correct answer");
+         
+         break;
+    
+     case 'NO','N':
+         console.log('Wrong answer');
+         alert("Wrong answer");
+         break;
+    
+ }
+
+
+
+ let Q4 = prompt('In binary, is the answer of the following question correct  10+1000=1010?');
+
+ switch (Q4.toUpperCase()) {
+     case 'YES','Y':
+         console.log('Correct answer ');
+         alert("correct answer");
+         break;
+    
+     case 'NO','N':
+         console.log('Wrong answer');
+         alert("Wrong answer");
+         break;
+    
+ }
+
+
+
+
+ let Q5 = prompt('Letters are not converted to binary in a computer like a numbers ?');
+
+ switch (Q5.toUpperCase()) {
+     case  'NO','N':
+         console.log('Correct answer ');
+         alert("correct answer");
+         break;
+    
+     case 'YES','Y':
+         console.log('Wrong answer');
+         alert("Wrong answer");
+         break;
+    
+ }
+
+    
+ alert("You are amazing! thank you " + username );
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lab2-201
+
+*I had a problem with adding the social media icon and looking for a solution on the W3school site and I took the icon's CSS file and put it on my site in addition to my CSS file for the other elements in my site*
