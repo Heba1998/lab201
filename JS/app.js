1fcdfd391f31a42d16abf5d6bbcf75ec21af0920
Heba1998 <hebaalmomani1998@gmail.com>
2021-04-27 16:13:34 +0300

    lab03

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,33 @@
  'use strict';
-
+ let score=0;
  let username =prompt('what is your name ?');
- console.log(username);
- alert(" Hello " + username + " ! My name is Heba, Let's start with a short quiz not to determine your level but only to increase your knowledge ^.^ , please answer them for just yes/no ");
+ while(!username){
+     username=prompt("please, enter your name")
+ }
 
+alert(" Hello " + username + " ! My name is Heba, Let's start with a short quiz not to determine your level but only to increase your knowledge ^.^ , please answer them for just yes/no ");
+
+
+
+console.log(username);
+
+ 
  let Q1 = prompt('Does the computer understand human language?');
 
  switch (Q1.toUpperCase()) {
      case 'N':
+     case 'NO':    
          console.log('Correct answer ');
          alert("correct answer");
+         score++;
          break;
 
-     case 'NO':
-            console.log('Correct answer ');
-            alert("correct answer");
-            break;    
-    
+
      case 'YES':
+     case 'Y':
          console.log('Wrong answer');
          alert("Wrong answer");
          break;
-
-    case 'Y':
-            console.log('Wrong answer');
-            alert("Wrong answer");
-            break;         
-    
  }
 
 
@@ -34,26 +35,18 @@
 
  switch (Q2.toUpperCase()) {
      case 'YES':
+     case 'Y':
          console.log('Correct answer ');
          alert("correct answer");
+         score++;
          break;
 
-    case 'Y':
-            console.log('Correct answer ');
-            alert("correct answer");
-            break;        
-    
+
      case 'NO':
+     case 'N':
          console.log('Wrong answer');
          alert("Wrong answer");
-         break;
-    
-
-    case 'N':
-            console.log('Wrong answer');
-            alert("Wrong answer");
-            break;
-       
+         break;  
  }
 
 
@@ -61,29 +54,17 @@
 
  switch (Q3.toUpperCase()) {
      case 'YES':
-         console.log('Correct answer ');
-         alert("correct answer");
-         
-         break;
-
-
      case 'Y':
          console.log('Correct answer ');
          alert("correct answer");
-         
-         break;
-    
-     case 'NO':
-         console.log('Wrong answer');
-         alert("Wrong answer");
+         score++;
          break;
 
-    case 'N':
+     case 'NO':
+     case 'N':
          console.log('Wrong answer');
          alert("Wrong answer");
-         break;
-    
-    
+         break 
  }
 
 
@@ -92,26 +73,18 @@
 
  switch (Q4.toUpperCase()) {
      case 'YES':
-         console.log('Correct answer ');
-         alert("correct answer");
-         break;
-
      case 'Y':
          console.log('Correct answer ');
          alert("correct answer");
+         score++;
          break;
-    
+
+
      case 'NO':
-         console.log('Wrong answer');
-         alert("Wrong answer");
-         break;
-
-
      case 'N':
          console.log('Wrong answer');
          alert("Wrong answer");
-         break;
-    
+         break; 
  }
 
 
@@ -121,29 +94,66 @@
 
  switch (Q5.toUpperCase()) {
      case  'NO':
-         console.log('Correct answer ');
+     case  'N' :
+        console.log('Correct answer ');
          alert("correct answer");
-         break;
-    
-
-     case  'N':
-         console.log('Correct answer ');
-         alert("correct answer");
+         score++;
          break;
     
 
      case 'YES':
+     case 'Y':
          console.log('Wrong answer');
          alert("Wrong answer");
          break;
-
-
-      case 'Y':
-            console.log('Wrong answer');
-            alert("Wrong answer");
-            break;
-    
  }
 
+
+ let Q6 = prompt('In this question, please, enter a number, Guess the number in the decimal corresponds to this representation in the binary 0100101?');
+ 
+ Q6 = Number(Q6);
+ console.log(Q6);
+ var i=0; 
+ while (i < 4) {
+    if(Q6==37) {
+        alert("correct, you are awesome");
+    score++;
+break; }
+       
+        else if (Q6 < 37){
+    Q6= prompt("No, try to higher number");
+    break;
+} else{
+    Q6= prompt("No, try to lower number");
+    ;
+}
+i++;
+}
+alert("the correct answer is :37")
+
+
+
+    let myfavnum = [99, 65, 9, 100, 55, 8, 4, 5, 25];
+
+    let Q7 = prompt ("last question, Guess my favorite number from this list " + myfavnum);
+
+    Q7 = Number(Q7);
+    console.log(Q7);
+    var i=0; 
+    while (i < 6) {
+    if (Q7==25) {alert("correct, you are awesome");
+    score++;
+break; }
+          
+          else{
+           
+                  Q7= prompt("No, you remain " +(5-i)+ " attempts to guess the correct answer, so try again ");
+              }
+            
+           i++;
+
+}
+alert("my favorite number is :25")
     
- alert("You are amazing! thank you " + username );
+ alert("your score is : " + score + ".......You are amazing! thank you " + username );
+
