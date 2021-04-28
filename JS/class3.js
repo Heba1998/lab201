4c09c9aef7b5da711267647eae9493c1e626beb6
Heba1998 <hebaalmomani1998@gmail.com>
2021-04-28 14:30:13 +0300

    qusai and heba

--- JS/class3.js
+++ JS/class3.js
@@ -1,157 +1,157 @@
 'use strict';
-// let score=0;
+let score=0;
 
-// function f1(){
-// let username =prompt('what is your name ?');
-// while(!username){
-//     username=prompt("please, enter your name")
-// }
+function f1(){
+let username =prompt('what is your name ?');
+while(!username){
+    username=prompt("please, enter your name")
+}
 
-// alert(" Hello " + username + " ! My name is Heba, Let's start with a short quiz not to determine your level but only to increase your knowledge ^.^ , please answer them for just yes/no ");
-
-
-
-// console.log(username);
-// }
-// f1();
-
-
-// function f2 (){
-
-// let Q1 = prompt('Does the computer understand human language?');
-
-// switch (Q1.toUpperCase()) {
-//     case 'N':
-//     case 'NO':    
-//         console.log('Correct answer ');
-//         alert("correct answer");
-//         score++;
-//         break;
-
-
-//     case 'YES':
-//     case 'Y':
-//         console.log('Wrong answer');
-//         alert("Wrong answer");
-//         break;
-// }
-// }
-// f2();
-
-
-// function f3(){
-// let Q2 = prompt('Is 1 yes or no in binary?');
-
-// switch (Q2.toUpperCase()) {
-//     case 'YES':
-//     case 'Y':
-//         console.log('Correct answer ');
-//         alert("correct answer");
-//         score++;
-//         break;
-
-
-//     case 'NO':
-//     case 'N':
-//         console.log('Wrong answer');
-//         alert("Wrong answer");
-//         break;  
-// }
-// }
-// f3();
-
-
-// function f4 (){
-// let Q3 = prompt('2 is 10 in binary?');
-
-// switch (Q3.toUpperCase()) {
-//     case 'YES':
-//     case 'Y':
-//         console.log('Correct answer ');
-//         alert("correct answer");
-//         score++;
-//         break;
-
-//     case 'NO':
-//     case 'N':
-//         console.log('Wrong answer');
-//         alert("Wrong answer");
-//         break 
-// }
-// }
-// f4();
-
-
-// function f5 () {
-// let Q4 = prompt('In binary, is the answer of the following question correct  10+1000=1010?');
-
-// switch (Q4.toUpperCase()) {
-//     case 'YES':
-//     case 'Y':
-//         console.log('Correct answer ');
-//         alert("correct answer");
-//         score++;
-//         break;
-
-
-//     case 'NO':
-//     case 'N':
-//         console.log('Wrong answer');
-//         alert("Wrong answer");
-//         break; 
-// }
-// }
-// f5();
+alert(" Hello " + username + " ! My name is Heba, Let's start with a short quiz not to determine your level but only to increase your knowledge ^.^ , please answer them for just yes/no ");
 
 
 
-// function f6(){
+console.log(username);
+}
+f1();
 
-// let Q5 = prompt('Letters are not converted to binary in a computer like a numbers ?');
 
-// switch (Q5.toUpperCase()) {
-//     case  'NO':
-//     case  'N' :
-//        console.log('Correct answer ');
-//         alert("correct answer");
-//         score++;
-//         break;
+function f2 (){
+
+let Q1 = prompt('Does the computer understand human language?');
+
+switch (Q1.toUpperCase()) {
+    case 'N':
+    case 'NO':    
+        console.log('Correct answer ');
+        alert("correct answer");
+        score++;
+        break;
+
+
+    case 'YES':
+    case 'Y':
+        console.log('Wrong answer');
+        alert("Wrong answer");
+        break;
+}
+}
+f2();
+
+
+function f3(){
+let Q2 = prompt('Is 1 yes or no in binary?');
+
+switch (Q2.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        console.log('Correct answer ');
+        alert("correct answer");
+        score++;
+        break;
+
+
+    case 'NO':
+    case 'N':
+        console.log('Wrong answer');
+        alert("Wrong answer");
+        break;  
+}
+}
+f3();
+
+
+function f4 (){
+let Q3 = prompt('2 is 10 in binary?');
+
+switch (Q3.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        console.log('Correct answer ');
+        alert("correct answer");
+        score++;
+        break;
+
+    case 'NO':
+    case 'N':
+        console.log('Wrong answer');
+        alert("Wrong answer");
+        break 
+}
+}
+f4();
+
+
+function f5 () {
+let Q4 = prompt('In binary, is the answer of the following question correct  10+1000=1010?');
+
+switch (Q4.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        console.log('Correct answer ');
+        alert("correct answer");
+        score++;
+        break;
+
+
+    case 'NO':
+    case 'N':
+        console.log('Wrong answer');
+        alert("Wrong answer");
+        break; 
+}
+}
+f5();
+
+
+
+function f6(){
+
+let Q5 = prompt('Letters are not converted to binary in a computer like a numbers ?');
+
+switch (Q5.toUpperCase()) {
+    case  'NO':
+    case  'N' :
+       console.log('Correct answer ');
+        alert("correct answer");
+        score++;
+        break;
    
 
-//     case 'YES':
-//     case 'Y':
-//         console.log('Wrong answer');
-//         alert("Wrong answer");
-//         break;
-// }
-// }
-// f6();
+    case 'YES':
+    case 'Y':
+        console.log('Wrong answer');
+        alert("Wrong answer");
+        break;
+}
+}
+f6();
 
 
-// function f7 (){
-// let Q6 = prompt('In this question, please, enter a number, Guess the number in the decimal corresponds to this representation in the binary 0100101?');
+function f7 (){
+let Q6 = prompt('In this question, please, enter a number, Guess the number in the decimal corresponds to this representation in the binary 0100101?');
 
-// Q6 = Number(Q6);
-// console.log(Q6);
-// var i=0; 
-// while (i < 4) {
-//    if(Q6==37) {
-//        alert("correct, you are awesome");
-//    score++;
-// break; }
+Q6 = Number(Q6);
+console.log(Q6);
+var i=0; 
+while (i < 4) {
+   if(Q6==37) {
+       alert("correct, you are awesome");
+   score++;
+break; }
       
-//        else if (Q6 < 37){
-//    Q6= prompt("No, too low");
+       else if (Q6 < 37){
+   Q6= prompt("No, too low");
 
-// } else{
-//    Q6= prompt("No, too high");
+} else{
+   Q6= prompt("No, too high");
    
-// }
-// i++;
-// }
-// alert("the correct answer is :37")
-// }
-// f7();
+}
+i++;
+}
+alert("the correct answer is :37")
+}
+f7();
 
 
 
